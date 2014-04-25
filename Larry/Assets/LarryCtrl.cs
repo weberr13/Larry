68eb273fb39fab648b4532d40fbe00d942693da0
Isaac Shapira <fresheyeball@gmail.com>
2014-04-25 00:04:09 -0600

    tree

---
+++ Larry/Assets/LarryCtrl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LarryCtrl : MonoBehaviour {
+
+	public float Speed = 0f;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+}
